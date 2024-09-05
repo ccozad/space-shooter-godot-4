985909e5f1f0e7317426bf460e94f7e379118fe7
ccozad <ccozad@gmail.com>
2024-09-04 23:34:14 -0700

    Boundaries, scrolling starfield background

--- main.gd
+++ main.gd
@@ -6,9 +6,16 @@ extends Node3D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	GameManager.set_boundary(
+		$"Boundary/LeftWall".position.x,
+		$"Boundary/RightWall".position.x,
+		$"Boundary/TopWall".position.z,
+		$"Boundary/BottomWall".position.z
+	)
+	GameManager.spawn_stars(self)
 	debug.init(ship_1)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	GameManager.process_background(self, delta)
--- scenes/debug.gd
+++ scenes/debug.gd
@@ -5,6 +5,7 @@ var FPS = 0
 var draw_calls = 0
 var frame_time = 0
 var VRAM = 0
+var small_star_count = 0
 var player
 var data
 
@@ -21,10 +22,12 @@ func _process(delta):
 		draw_calls = RenderingServer.get_rendering_info(RenderingServer.RENDERING_INFO_TOTAL_DRAW_CALLS_IN_FRAME)
 		frame_time = delta
 		VRAM = RenderingServer.get_rendering_info(RenderingServer.RENDERING_INFO_VIDEO_MEM_USED) / 1024.0 / 1024.0
+		small_star_count = get_tree().get_nodes_in_group("small_star").size()
 		data = "FPS: " + str(FPS) + "\n" + \
 		"Draw calls: " + str(draw_calls) + "\n" + \
 		"Frame time: " + "%0.1f" % (frame_time * 1000) + " ms\n" + \
-		"VRAM: " + "%0.1f" % VRAM + " MB\n"
+		"VRAM: " + "%0.1f" % VRAM + " MB\n" + \
+		"small stars: " + str(small_star_count) + "\n"
 		
 		if Utils.is_valid_node(player):
 			data += "Position: " + str(player.global_position) + "\n" + \
