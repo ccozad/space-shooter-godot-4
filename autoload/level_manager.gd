2ca646b8b551985fcf36b97caec202547955f759
ccozad <ccozad@gmail.com>
2024-09-26 01:14:29 -0700

    Compile shaders code, not enabled yet

--- autoload/level_manager.gd
+++ autoload/level_manager.gd
@@ -6,3 +6,16 @@ func load_level(level):
 	print("Loading level: " + level)
 	current_level = load("res://levels/" + level + ".gd").new()
 	return current_level
+
+func compile_shaders(node, scenes):
+	print("Compiling shaders")
+	var materials = Utils.get_all_materials_from_scene(scenes)
+	for key in materials:
+		var new_mesh: MeshInstance3D = MeshInstance3D.new()
+		new_mesh.add_to_group("mesh")
+		new_mesh.set_surface_override_material(0, materials[key])
+		new_mesh.position = Vector3(
+			GameManager.boundary.left + GameManager.boundary_margin / 2.0,
+			0,
+			GameManager.boundary.bottom - GameManager.boundary_margin / 2.0)
+		node.call_deferred("add_child", new_mesh)
--- autoload/utils.gd
+++ autoload/utils.gd
@@ -16,3 +16,35 @@ func get_all_children(in_node, array = []):
 
 func get_random_x_in_viewport(padding = 0.0):
 	return randf_range(GameManager.boundary.left + padding, GameManager.boundary.right - padding)
+
+ 
+func get_all_materials_from_scene(scenes):
+	var materials = {}
+	for scene in scenes:
+		for material in get_all_materials(scene.instantiate()):
+			materials[str(material.get_rid().get_id())] = material
+	return materials
+
+func get_all_materials(source_node):
+	var materials
+	for child in get_all_children(source_node):
+		if child is MeshInstance3D:
+			var mesh: MeshInstance3D = child as MeshInstance3D
+			for i in mesh.get_surface_override_material_count():
+				var material = mesh.get_surface_override_material(i)
+				if material != null and is_instance_valid(material):
+					materials.append(material)
+		
+		if child is GPUParticles3D:
+			var particle: GPUParticles3D = child as GPUParticles3D
+			var material = particle.draw_pass_1.surface_get_material(0)
+			if material != null and is_instance_valid(material):
+				materials.append(material)
+		
+		if child is CPUParticles3D:
+			var particle: CPUParticles3D = child as CPUParticles3D
+			var material = particle.draw_pass_1.surface_get_material(0)
+			if material != null and is_instance_valid(material):
+				materials.append(material)
+		
+	return materials
