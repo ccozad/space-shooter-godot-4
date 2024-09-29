e51fd60cf37e97b4e33d7144a956af88371e310f
ccozad <ccozad@gmail.com>
2024-09-29 12:55:45 -0700

    Working on the settings screen

--- scenes/menu.gd
+++ scenes/menu.gd
@@ -27,3 +27,7 @@ func _on_start_button_pressed() -> void:
 
 func _on_exit_button_pressed() -> void:
 	get_tree().quit()
+
+
+func _on_settings_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/settings.tscn")
