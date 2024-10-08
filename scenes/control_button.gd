4b49129a153e96f67872306cc0d40dd612df6280
ccozad <ccozad@gmail.com>
2024-10-08 00:31:06 -0700

    Fix primary and secondary key bindings for move up

--- scenes/control_button.gd
+++ scenes/control_button.gd
@@ -5,14 +5,14 @@ extends Button
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	display_current_key()
 
 func display_current_key():
 	var action_events = InputMap.action_get_events(action)
 	var current_key = ""
 	if primary and action_events.size() > 0:
 		current_key = action_events[0].as_text()
-	elif not primary and action_events[1].size() > 1:
+	elif not primary and action_events.size() > 1:
 		current_key = action_events[1].as_text()
 	text = current_key
 
