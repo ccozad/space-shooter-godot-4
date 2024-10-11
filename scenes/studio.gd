d6dad85dd397ca189db264554881485f2c17c3e3
ccozad <ccozad@gmail.com>
2024-10-11 01:38:36 -0700

    Pressing the escape key will skip the studio screen

--- scenes/studio.gd
+++ scenes/studio.gd
@@ -9,5 +9,12 @@ func _ready() -> void:
 	GameManager.release_mouse()
 	animation_player.play("author_fade_in_out")
 
+func _input(_event):
+	if Input.is_action_just_pressed("main_menu"):
+		launch_menu_scene()
+
 func _on_animation_player_animation_finished(anim_name: StringName) -> void:
+	launch_menu_scene()
+
+func launch_menu_scene():
 	get_tree().change_scene_to_file("res://scenes/menu.tscn")
